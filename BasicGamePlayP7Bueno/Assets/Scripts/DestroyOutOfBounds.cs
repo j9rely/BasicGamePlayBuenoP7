890061b731ac38d4764d189643c57efe352f2f77
j9rely <145072305+j9rely@users.noreply.github.com>
2024-03-22 15:21:23 -0700

    FINISH
    DONW

--- BasicGamePlayP7Bueno/Assets/Scripts/DestroyOutOfBounds.cs
+++ BasicGamePlayP7Bueno/Assets/Scripts/DestroyOutOfBounds.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class DestroyOutOfBounds : MonoBehaviour
 {
+
+    private float topBound = 30.0f;
+    private float lowerBound = -10.0f;
+    private float sideBound = 30.0f;
+    private GameManager gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
-    // Update is called once per frame
-    private float topBound = 30f;
-    private float lowerBound = -10f;
-    private float sideBound = 30;
-    private GameManager gameManager;
 
+
+    // Update is called once per frame
     void Update()
     {
         //If an object goes past the players view in the game, remove that object
@@ -33,7 +36,7 @@ public class DestroyOutOfBounds : MonoBehaviour
             gameManager.AddLives(-1);
             Destroy(gameObject);
         }
-        else if (transform.position.x < -sideBound) ;
+        else if (transform.position.x < -sideBound)
         {
             gameManager.AddLives(-1);
             Destroy(gameObject);
